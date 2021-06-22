b177b0cfa84a0acf174d9a40627a7a817c107e2a
JensDeNijs <jensdenijs@hotmail.com>
2021-06-22 12:12:19 +0200

    Cleanup

--- Controller/InfoController.php
+++ Controller/InfoController.php
@@ -6,27 +6,6 @@ class InfoController
     //render function with both $_GET and $_POST vars available if it would be needed.
     public function render(array $GET, array $POST)
     {
-        /*$loader = new StudentLoader();
-        $allClasses = $loader->getStudents();
-
-        print_r($allClasses);
-        echo "<br>";
-        echo $allClasses[0]->getId();
-        echo "<br>";
-        var_dump($POST);
-        echo "<br>";
-        echo $POST['btn'];
-        echo "<br>";*/
-
-        if (isset($POST["btn"])) {
-            if($POST["btn"]==="student"){
-
-            }elseif($POST["btn"]==="class"){
-
-            }elseif($POST["btn"]==="teacher"){
-
-            }
-        }
 
         require 'View/info.php';
 
--- View/info.php
+++ View/info.php
@@ -3,36 +3,6 @@
         <h4>Info page</h4>
 
         <p><a href="index.php">Back to homepage</a></p>
-        <form action="" method="POST">
-            <p>
-                <button type="submit" name="btn" value="student">Student</button>
-                <button type="submit" name="btn" value="class">Class</button>
-                <button type="submit" name="btn" value="teacher">Teacher</button>
-            </p>
 
-        <table>
-            <?php
-            if (isset($POST["btn"])) {
-                if($POST["btn"]==="student"){
-                    echo '<tr><th>ID</th><th>Name</th><th>Email</th><th>ClassID</th></tr>';
-                    foreach ($data as $row) {
-                        echo '<tr><td>'.$row->getId().'</td><td>'.$row->getName().'</td><td>'.$row->getEmail().'</td><td>'.$row->getClassId().'</td><td><a href="" class="">Update</a></td><td><button type="" value="'.$row->getClassId().'">Delete</button></td></tr>';
-                    }
-                }elseif($POST["btn"]==="class"){
-                    echo '<tr><th>ID</th><th>Name</th><th>Location</th><th>TeacherID</th></tr>';
-                    foreach ($data as $row) {
-                        echo '<tr><td>'.$row->getId().'</td><td>'.$row->getName().'</td><td>'.$row->getLocation().'</td><td>'.$row->getTeacherId().'</td></tr>';
-                    }
-                }elseif($POST["btn"]==="teacher"){
-                    echo '<tr><th>ID</th><th>Name</th><th>Email</th></tr>';
-                    foreach ($data as $row) {
-                        echo '<tr><td>'.$row->getId().'</td><td>'.$row->getName().'</td><td>'.$row->getEmail().'</td></tr>';
-                    }
-                }
-            }
-
-            ?>
-        </table>
-        </form>
     </section>
 <?php require 'includes/footer.php' ?>
